1ae1b9fa46cd47a1eaeb7bb13bcaf02c382601ef
jinyoung1018 <jywsb1816@gmail.com>
2023-04-12 11:11:08 +0900

    툴바 라우터 링크 스타일링

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -19,5 +19,9 @@ export default {
 </script>
 
 <style>
+body{
+  padding: 0;
+  margin: 0;
 
+}
 </style>
--- vue-news/src/components/ToolBar.vue
+++ vue-news/src/components/ToolBar.vue
@@ -7,15 +7,19 @@
   </div>
 </template>
 
-<script>
-export default {
-
-}
-</script>
-
 <style scoped> 
 .header{
-    color: white
+    color: white;
+    background-color: #42b883;
+    display: flex;
+    padding: 8px;
+}
+.header .router-link-exact-active{
+    color: #35495e;
+}
+
+.header a{
+    color:white;
 }
 
 </style>
