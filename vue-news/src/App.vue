9441f02ee85fbe2f0ffa9bf84e79f69d97f88b40
jinyoung1018 <jywsb1816@gmail.com>
2023-04-17 18:00:37 +0900

    라우터 트랜지션

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
-    <!-- <ToolBar></ToolBar> -->
     <tool-bar></tool-bar>
+    <transition name="page">
     <router-view></router-view>
+  </transition>
   </div>
  
 </template>
@@ -23,5 +24,11 @@ body{
   padding: 0;
   margin: 0;
 
+}
+.page-enter-active, .page-leave-active {
+  transition: opacity .5s;
+}
+.page-enter, .page-leave-to{
+  opacity: 0;
 }
 </style>
