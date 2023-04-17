d000874d43441009abadb508e2b17bf4a155305d
jinyoung1018 <jywsb1816@gmail.com>
2023-04-17 23:48:07 +0900

    공통 컴포넌트 ListItem제작

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,35 +1,22 @@
 <template>
   <div>
-    <ul class="news-list">
-      <li v-for="item in this.$store.state.news" class="post">
-       <!-- 포인트 영역 -->
-        <div class="points">
-          {{ item.points }}
-        </div>
-        <!-- 기타 정보 영역 -->
-        <div>
-          <p class="news-title">
-            <a v-bind:href="item.url">
-              {{ item.title }}
-            </a>
-          </p>
-          <small class="link-text">
-            {{item.time_ago}} by
-            <router-link v-bind:to="`/user/${item.user}`" class="link-text"> {{ item.user }} </router-link>
-          </small>
-        </div>
-      </li>
-    </ul>
+    <list-item></list-item>
+    <!-- <ListItem></ListItem> -->
+    
    
   </div>
 </template>
 
 <script>
+import ListItem from '../components/ListItem.vue'
 // import axios from 'axios';
 // import { fetchNewsList } from '../api/index.js'
 
 
 export default {
+  components:{
+    ListItem,
+  }
     // data(){
     //     return{
     //         users:[]
@@ -50,8 +37,8 @@ export default {
     // },
 
 
-    created(){
-        this.$store.dispatch('FETCH_NEWS');
+    // created(){
+    //     this.$store.dispatch('FETCH_NEWS');
         // console.log(this);
 
         // fetchNewsList()
@@ -62,39 +49,8 @@ export default {
         // .catch(function(error){
         //     console.log(error);
 
-        // })
-    },
+    //     // })
+    // },
 
 }
 </script>
-
-<style scoped>
-.news-list{
-  margin:0;
-  padding: 0;
-}
-
-.post{
-  list-style: none;
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid #eee;
-}
-.points{
-  width: 80px;
-  height: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #42b883
-}
-
-.news-title{
-  margin:0;
-}
-.link-text{
-  color: #828282;
-
-}
-
-</style>
