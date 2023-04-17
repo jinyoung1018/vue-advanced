0d6470403bcca4a3c771d53db36bd837770e726e
jinyoung1018 <jywsb1816@gmail.com>
2023-04-17 23:16:02 +0900

    뉴스 리스트 스타일링

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -23,8 +23,21 @@ export default {
 body{
   padding: 0;
   margin: 0;
+}
+a{
+  text-decoration: none;
+  color:#344952;
+}
+a.router-link-exact-active{
+  text-decoration:underline ; 
+}
+a:hover{
+  color: #42b883;
+  text-decoration:underline ; 
+
 
 }
+
 .page-enter-active, .page-leave-active {
   transition: opacity .5s;
 }
